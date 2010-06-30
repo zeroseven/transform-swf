4a2127bd54ed9d00cb4fe7164d422e7759c51a05
smackay <smackay@e85137b9-a616-0410-b282-ce273dc723a7>
2010-06-30 12:16:49 +0000

    Modified to decode long headers.

--- src/main/java/com/flagstone/transform/text/TextSettings.java
+++ src/main/java/com/flagstone/transform/text/TextSettings.java
@@ -109,7 +109,10 @@ public final class TextSettings implements MovieTag {
      *             if an error occurs while decoding the data.
      */
     public TextSettings(final SWFDecoder coder) throws IOException {
-        coder.readUnsignedShort();
+        int length = coder.readUnsignedShort() & Coder.LENGTH_FIELD;
+        if (length == Coder.IS_EXTENDED) {
+            length = coder.readInt();
+        }
         identifier = coder.readUnsignedShort();
         rendering = coder.readByte();
         thickness = coder.readInt();
